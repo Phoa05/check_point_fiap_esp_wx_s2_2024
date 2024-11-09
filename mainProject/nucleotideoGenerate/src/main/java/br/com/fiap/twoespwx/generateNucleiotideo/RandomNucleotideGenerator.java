31c27841b55fb2350c0933e844087c8f1b53c1ea
Pedro Henrique <pedro.phoa1405@gmail.com>
2024-11-09 00:16:47 -0300

    Checkpoint 2 - criando lógica do metodo generate da classe RandomNucleotideGenerator

--- mainProject/nucleotideoGenerate/src/main/java/br/com/fiap/twoespwx/generateNucleiotideo/RandomNucleotideGenerator.java
+++ mainProject/nucleotideoGenerate/src/main/java/br/com/fiap/twoespwx/generateNucleiotideo/RandomNucleotideGenerator.java
@@ -3,9 +3,15 @@ package br.com.fiap.twoespwx.generateNucleiotideo;
 import java.util.Random;
 
 public class RandomNucleotideGenerator implements NucleotideoRandomGenerator {
+    private static final char[] NUCLEOTIDES = {'A', 'C', 'T', 'G'};
+    private final Random random = new Random();
 
     @Override
     public String generate(int sequenceSize) {
-        return "";
+        StringBuilder sequence = new StringBuilder(sequenceSize);
+        for (int i = 0; i < sequenceSize; i++) {
+            sequence.append(NUCLEOTIDES[random.nextInt(NUCLEOTIDES.length)]);
+        }
+        return sequence.toString();
     }
 }
